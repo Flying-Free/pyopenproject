ae548b3c3f9d3853c95a4afe5da3f6610dee4a8c
Marcelo Torrejón Manso <mtorrejonman@dxc.com>
2020-12-23 11:02:14 +0100

    Budgets by project

--- business/impl/command/project/list_budgets.py
+++ business/impl/command/project/list_budgets.py
@@ -2,17 +2,18 @@ from api_connection.connection import Connection
 from api_connection.exceptions.request_exception import RequestError
 from business.exception.business_error import BusinessError
 from business.impl.command.project.project_command import ProjectCommand
-from model.project import Project
+from model.budget import Budget
 
 
-class Find(ProjectCommand):
+class ListBudgets(ProjectCommand):
 
     def __init__(self, project):
         self.project = project
 
     def execute(self):
         try:
-            json_obj = Connection().get(f"{self.CONTEXT}/{self.project.id}")
-            return Project(json_obj)
+            json_obj = Connection().get(f"{self.CONTEXT}/{self.project.id}/budgets")
+            for budget in json_obj["_embedded"]["elements"]:
+                yield Budget(budget)
         except RequestError as re:
             raise BusinessError(f"Error finding project by id: {self.project.name}") from re
