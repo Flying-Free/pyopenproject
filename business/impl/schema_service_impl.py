696f01d25243ed4d386fed913775cc611e9c8a82
Marcelo Torrejón Manso <mtorrejonman@protonmail.com>
2020-12-30 12:56:28 +0100

    Clean schema

--- business/impl/schema_service_impl.py
+++ business/impl/schema_service_impl.py
@@ -1,19 +1,6 @@
-from business.impl.command.work_package.find_all import FindAll
-from business.impl.command.work_package.find_by_context import FindByContext
-from business.impl.command.work_package.find_by_id import FindById
+
 from business.schema_service import SchemaService
 
 
 class SchemaServiceImpl(SchemaService):
-
-    def find_all(self):
-        return FindAll().execute
-
-    def find_by_id(self, identifier):
-        return FindById(self.identifier).execute()
-
-    def find_by_context(self, context):
-        return FindByContext(context).execute()
-
-    # TODO: Review what params we need to create a new schema
-    def new_schema(self): raise NotImplementedError
+    pass
--- business/schema_service.py
+++ business/schema_service.py
@@ -1,18 +1,6 @@
-from abc import ABCMeta, abstractmethod
+from abc import ABCMeta
 
 
 class SchemaService:
     __metaclass__ = ABCMeta
 
-    @abstractmethod
-    def find_all(self): raise NotImplementedError
-
-    @abstractmethod
-    def find(self, identifier): raise NotImplementedError
-
-    @abstractmethod
-    def find_by_context(self, context): raise NotImplementedError
-
-    # TODO: Review what params we need to create a new schema
-    @abstractmethod
-    def new_schema(self): raise NotImplementedError
