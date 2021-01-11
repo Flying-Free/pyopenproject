102e8a1eb00462fa2e3180235d645b3b4c1610ac
Marcelo Torrejón Manso <mtorrejonman@protonmail.com>
2021-01-11 17:17:54 +0100

    Activity service change

--- tests/activity_service_test.py
+++ tests/activity_service_test.py
@@ -1,14 +1,18 @@
+import json
 import unittest
 
 from business.activity_service import ActivityService
 
 
 class ActivityServiceTestCase(unittest.TestCase):
-    actSer = ActivityService()
 
+
+    def setUp(self):
+        self.actSer = ActivityService()
+        self.activity = json.loads('/data/activity.json')
 
     def test_find_activity(self):
-        self.assertNotNull(self.actSer.find(Activity()))
+        self.assertNotNull(self.actSer.find(self.activity))
 
     def test_update_activity(self):
-        self.assertNotNull(self.actSer.update(Activity()))
+        self.assertNotNull(self.actSer.update(self.activity))
