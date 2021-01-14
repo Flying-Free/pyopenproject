042965c192958e9a8473281fb337e11f9c822a96
AlanPadi95 <alanpadierna9595@gmail.com>
2021-01-14 13:54:09 +0100

    Configuration tests

--- tests/configuration_service_test.py
+++ tests/configuration_service_test.py
@@ -14,3 +14,9 @@ class ConfigurationServiceTestCase(unittest.TestCase):
 
     def test_find(self):
         self.assertIsNotNone(self.confSer.find())
+
+    def test_same_config(self):
+        current = self.confSer.find()
+        expected = self.configuration
+        self.assertEqual(expected.__dict__, current.__dict__)
+
