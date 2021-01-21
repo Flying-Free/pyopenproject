4237ea6e68053c816143de4d00626bfd5c4996a1
AlanPadi95 <alanpadierna9595@gmail.com>
2021-01-21 17:22:46 +0100

    Some tests for Project

--- business/services/impl/command/project/delete.py
+++ business/services/impl/command/project/delete.py
@@ -1,4 +1,3 @@
-import model.project as p
 from api_connection.exceptions.request_exception import RequestError
 from api_connection.requests.delete_request import DeleteRequest
 from business.exception.business_error import BusinessError
@@ -13,7 +12,6 @@ class Delete(ProjectCommand):
 
     def execute(self):
         try:
-            json_obj = DeleteRequest(self.connection, f"{self.CONTEXT}/{self.project.id}").execute()
-            return p.Project(json_obj)
+            DeleteRequest(self.connection, f"{self.CONTEXT}/{self.project.id}").execute()
         except RequestError as re:
             raise BusinessError(f"Error deleting project: {self.project.name}") from re
--- tests/test_cases/project_service_test.py
+++ tests/test_cases/project_service_test.py
@@ -1,5 +1,6 @@
 import json
 
+from business.exception.business_error import BusinessError
 from model.project import Project
 from tests.test_cases.openproject_test_case import OpenProjectTestCase
 
@@ -9,7 +10,7 @@ class ProjectServiceTestCase(OpenProjectTestCase):
     def setUp(self):
         super().setUp()
         self.proSer = self.factory.get_project_service()
-        with open('../data/project.json') as f:
+        with open("../data/project.json") as f:
             self.project = Project(json.load(f))
 
     def test_find(self):
@@ -17,14 +18,60 @@ class ProjectServiceTestCase(OpenProjectTestCase):
         self.assertEqual(self.project.__dict__, current.__dict__)
 
     def test_update(self):
-        self.assertIsNotNone(self.proSer.update(self.project))
+        # TODO:Error Traceback (most recent call last): File 
+        #  "($HOME)\Documents\GitHub\python-openproject-api\api_connection\request.py", line 24, 
+        #  in execute response.raise_for_status() File "C:\Python39\lib\site-packages\requests\models.py", line 943, 
+        #  in raise_for_status raise HTTPError(http_error_msg, response=self) requests.exceptions.HTTPError: 500 
+        #  Server Error: Internal Server Error for url: http://127.0.0.1:8080/api/v3/projects/2
+        #  The above exception was the direct cause of the following exception:
+        #  Traceback (most recent call last): File
+        #  "($HOME)\Documents\GitHub\python-openproject-api\business\services\impl\command\project
+        #  \update.py", line 18, in execute json_obj = PatchRequest(connection=self.connection,
+        #  File "($HOME)\Documents\GitHub\python-openproject-api\api_connection\request.py", line 47,
+        #  in execute raise RequestError(f"Error running request with the URL: "{self.connection.url_base +
+        #  self.context}"." + api_connection.exceptions.request_exception.RequestError: Error running request with the
+        #  URL: "http://127.0.0.1:8080/api/v3/projects/2". {"_type":"Error",
+        #  "errorIdentifier":"urn:openproject-org:api:v3:errors:InternalServerError","message":"An internal error has
+        #  occured. undefined method `fetch" for #<String:0x000055e39a848248>"}
+        #  The above exception was the direct cause of the following exception:
+        #  Traceback (most recent call last): File "C:\Python39\lib\unittest\case.py", line 59, in testPartExecutor
+        #  yield File "C:\Python39\lib\unittest\case.py", line 593, in run self._callTestMethod(testMethod) File
+        #  "C:\Python39\lib\unittest\case.py", line 550, in _callTestMethod method() File
+        #  "($HOME)\Documents\GitHub\python-openproject-api\tests\test_cases\project_service_test.py",
+        #  line 23, in test_update current = self.proSer.update(current) File
+        #  "($HOME)\Documents\GitHub\python-openproject-api\business\services\impl\project_service_impl
+        #  .py", line 30, in update return Update(self.connection, project).execute() File
+        #  "($HOME)\Documents\GitHub\python-openproject-api\business\services\impl\command\project
+        #  \update.py", line 23, in execute raise BusinessError(f"Error updating project: {self.project.name}") from
+        #  re business.exception.business_error.BusinessError: Error updating project: Scrum project
+
+        self.project = self.proSer.find(self.project)
+        current = self.proSer.find(self.project)
+        current.status = "new"
+        current = self.proSer.update(current)
+        self.assertEqual("new", current.status)
+        current.status = self.project.status
+        current = self.proSer.update(current)
+        self.assertEqual(self.project.status, current.status)
 
     def test_delete(self):
-        self.assertIsNotNone(self.proSer.delete(self.project))
+        current = self.proSer.find(self.project)
+        with open("../data/scrum_project.json") as f:
+            expected = Project(json.load(f))
+        self.assertEqual(expected.__dict__, current.__dict__)
+        self.proSer.delete(self.project)
+        # current = self.proSer.find(self.project)
+        with self.assertRaises(BusinessError):
+            self.proSer.find(self.project)
 
     def test_find_all(self):
         projects = self.proSer.find_all()
         self.assertEqual(2, len(projects))
+
+    def test_find_projects_with_filters(self):
+        # TODO: Review filters
+        projects = self.proSer.find_all(filters="[{ \"active\": { \"operator\": \"=\", \"values\": [\"false\"] }\" }]")
+        self.assertEqual(0, len(projects))
 
     def test_create(self):
         self.assertIsNotNone(self.proSer.create(self.project))
