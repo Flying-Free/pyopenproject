7c81ac8ce30827b078bedc49fa158c595ce53743
AlanPadi95 <alanpadierna9595@gmail.com>
2021-02-22 17:02:07 +0100

    FIXED: Documentation issues

--- util/URLParameter.py
+++ util/URLParameter.py
@@ -1,7 +1,8 @@
 class URLParameter:
 
     def __init__(self, name, value):
-        """ Constructor for class URLParameter
+        """
+        Constructor for class URLParameter
 
         :param name: Parameter name
         :param value: Parameter value
@@ -10,8 +11,7 @@ class URLParameter:
         self.value = value
 
     def __str__(self) -> str:
-        """
-        Converts the URL parameter to a string
+        """ Converts the URL parameter to a string
 
         :return: The parameter as a string to insert it in an URL
         """
