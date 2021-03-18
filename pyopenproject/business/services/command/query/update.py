c67e983ad3ec44199c218dfc8a1e4237973062d7
Marcelo T.M <mtorrejonman@protonmail.com>
2021-03-18 14:51:29 +0100

    Query update changes

--- pyopenproject/business/services/command/query/update.py
+++ pyopenproject/business/services/command/query/update.py
@@ -1,8 +1,10 @@
-from pyopenproject.api_connection.exceptions.request_exception import RequestError
-from pyopenproject.api_connection.requests.patch_request import PatchRequest
-from pyopenproject.business.exception.business_error import BusinessError
-from pyopenproject.business.services.command.query.query_command import QueryCommand
-from pyopenproject.model.query import Query
+from contextlib import suppress
+
+from pyopenquery.api_connection.exceptions.request_exception import RequestError
+from pyopenquery.api_connection.requests.patch_request import PatchRequest
+from pyopenquery.business.exception.business_error import BusinessError
+from pyopenquery.business.services.command.query.query_command import QueryCommand
+from pyopenquery.model.query import Query
 
 
 class Update(QueryCommand):
@@ -13,10 +15,37 @@ class Update(QueryCommand):
 
     def execute(self):
         try:
+            query_id = self.query.id
+            self.__remove_readonly_attributes()
             json_obj = PatchRequest(connection=self.connection,
                                     headers={"Content-Type": "application/json"},
-                                    context=f"{self.CONTEXT}/{self.query.id}",
+                                    context=f"{self.CONTEXT}/{query_id}",
                                     json=self.query.__dict__).execute()
             return Query(json_obj)
         except RequestError as re:
             raise BusinessError(f"Error updating query by id: {self.query.id}") from re
+        
+    def __remove_readonly_attributes(self):
+        with suppress(KeyError): del self.query.__dict__["_links"]["self"]
+        with suppress(KeyError): del self.query.__dict__["_links"]["user"]
+        with suppress(KeyError): del self.query.__dict__["_links"]["project"]
+        with suppress(KeyError): del self.query.__dict__["_links"]["columns"]
+        with suppress(KeyError): del self.query.__dict__["_links"]["highlightedAttributes"]
+        with suppress(KeyError): del self.query.__dict__["_links"]["sortBy"]
+        with suppress(KeyError): del self.query.__dict__["_links"]["groupBy"]
+        with suppress(KeyError): del self.query.__dict__["_links"]["results"]
+        with suppress(KeyError): del self.query.__dict__["_links"]["schema"]
+
+        with suppress(KeyError): del self.query.__dict__["id"]
+        with suppress(KeyError): del self.query.__dict__["name"]
+        with suppress(KeyError): del self.query.__dict__["filters"]
+        with suppress(KeyError): del self.query.__dict__["sums"]
+        with suppress(KeyError): del self.query.__dict__["timelineVisible"]
+        with suppress(KeyError): del self.query.__dict__["timelineLabels"]
+        with suppress(KeyError): del self.query.__dict__["highlightingMode"]
+        with suppress(KeyError): del self.query.__dict__["showHierarchies"]
+        with suppress(KeyError): del self.query.__dict__["hidden"]
+        with suppress(KeyError): del self.query.__dict__["public"]
+        with suppress(KeyError): del self.query.__dict__["starred"]
+        with suppress(KeyError): del self.query.__dict__["createdAt"]
+        with suppress(KeyError): del self.query.__dict__["updatedAt"]
