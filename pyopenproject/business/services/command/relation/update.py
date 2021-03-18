9772ada5745c0309acba4738cc51a39845bbf84b
Marcelo T.M <mtorrejonman@protonmail.com>
2021-03-18 14:04:30 +0100

    Relation update changes

--- pyopenproject/business/services/command/relation/update.py
+++ pyopenproject/business/services/command/relation/update.py
@@ -1,3 +1,5 @@
+from contextlib import suppress
+
 from pyopenproject.api_connection.exceptions.request_exception import RequestError
 from pyopenproject.api_connection.requests.patch_request import PatchRequest
 from pyopenproject.business.exception.business_error import BusinessError
@@ -13,10 +15,21 @@ class Update(RelationCommand):
 
     def execute(self):
         try:
+            relation_id = self.relation.id
+            self.__remove_readonly_attributes()
             json_obj = PatchRequest(connection=self.connection,
                                     headers={"Content-Type": "application/json"},
-                                    context=f"{self.CONTEXT}/{self.relation.id}",
+                                    context=f"{self.CONTEXT}/{relation_id}",
                                     json=self.relation.__dict__).execute()
             return rel.Relation(json_obj)
         except RequestError as re:
-            raise BusinessError(f"Error updating relation by id: {self.relation.id}") from re
+            raise BusinessError(f"Error updating relation by id: {relation_id}") from re
+
+    def __remove_readonly_attributes(self):
+        with suppress(KeyError): del self.relation.__dict__["_links"]["self"]
+        with suppress(KeyError): del self.relation.__dict__["_links"]["schema"]
+        with suppress(KeyError): del self.relation.__dict__["_links"]["from"]
+        with suppress(KeyError): del self.relation.__dict__["_links"]["to"]
+        with suppress(KeyError): del self.relation.__dict__["id"]
+        with suppress(KeyError): del self.relation.__dict__["name"]
+        with suppress(KeyError): del self.relation.__dict__["reverseType"]
