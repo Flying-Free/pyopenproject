8eda0c5928cd2d7b42c9a78e8589b36e307ee2ce
Thomas Geffert <thomas.geffert@arcutronix.com>
2021-06-24 13:37:46 +0200

    Fixed find_categories in project_service
    FindCategories function was already prepared but contained a bug and was not
    implemented in project_service.

--- pyopenproject/business/services/command/project/find_categories.py
+++ pyopenproject/business/services/command/project/find_categories.py
@@ -3,7 +3,7 @@ from pyopenproject.api_connection.requests.get_request import GetRequest
 from pyopenproject.business.exception.business_error import BusinessError
 from pyopenproject.business.services.command.find_list_command import FindListCommand
 from pyopenproject.business.services.command.project.project_command import ProjectCommand
-from pyopenproject.model import Category
+from pyopenproject.model.category import Category
 
 
 class FindCategories(ProjectCommand):
