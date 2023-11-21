20fd068bd3570053f12bf53d795cba9fbc2840cf
legatusL <legatus.laboravi@gmail.com>
2023-11-21 18:24:18 +0100

    [CREATED] new features for notifications

--- pyopenproject/business/services/notification_service_impl.py
+++ pyopenproject/business/services/notification_service_impl.py
@@ -1,6 +1,10 @@
 from pyopenproject.business.notification_service import NotificationService
 from pyopenproject.business.services.command.notification.find_all import FindAll
-
+from pyopenproject.business.services.command.notification.find import Find
+from pyopenproject.business.services.command.notification.read_all import ReadAll
+from pyopenproject.business.services.command.notification.unread_all import UnReadAll
+from pyopenproject.business.services.command.notification.read import Read
+from pyopenproject.business.services.command.notification.unread import UnRead
 
 class NotificationServiceImpl(NotificationService):
 
@@ -17,7 +21,7 @@ class NotificationServiceImpl(NotificationService):
         Args:
             notification_id (int): The notification that is searched by id
         """
-        pass
+        return Find(self.connection, notification_id).execute()
 
     def find_all(self, offset=None, page_size=None, filters=None,
                  sort_by=None, group_by=None):
@@ -36,20 +40,50 @@ class NotificationServiceImpl(NotificationService):
         """
         return FindAll(self.connection, offset, page_size, filters, sort_by, group_by).execute()
 
-    def read_all(filter=None):
-        pass
+    def read_all(self, filters=None):
+        """
+        Marks the whole notification collection as read. The collection contains only elements the authenticated user can see, and can be further reduced with filters.
+        Args:
+            filters (dict(JSON), optional): Filters. Defaults to None.
 
-    def unread_all(filter=None):
-        pass
+        Returns:
+            str: If the resource was created returns the response (204), else throw an error
+        """
+        return ReadAll(self.connection, filters).execute()
 
-    def read_notification(notification_id=None):
-        pass
+    def unread_all(self, filters=None):
+        """
+        Marks the whole notification collection as unread. The collection contains only elements the authenticated user can see, and can be further reduced with filters.
+        Args:
+            filters (list, optional): A list of dictionaries of filtres. Defaults to None.
+
+        Returns:
+            _type_: _description_
+        """
+        return UnReadAll(self.connection, filters).execute()
+
+    def read_notification(notification_id=None, filters=None):
+        """
+        Marks the given notification as read.
+        Args:
+            notification_id (integer, optional): _description_. Defaults to None.
+            filters (list, optional): _description_. Defaults to None.
+
+        Returns:
+            _type_: _description_
+        """
+        return Read(self.connection, notification_id, filters)
 
     def unread_notification(notification_id=None):
-        pass
+        """
 
-    def unread_notification(notification_id=None):
-        pass
+        Args:
+            notification_id (_type_, optional): _description_. Defaults to None.
+
+        Returns:
+            _type_: _description_
+        """
+        return UnRead(self.connection, notification_id, filters)
 
     def find_notification_detail(
             notification_id=None, detail_id=None):
